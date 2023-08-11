b5cbb1a31fba410d36016dd2983905a1d1d6d4e2
Steam Deck User <deck@steamdeck.station>
2023-08-11 22:18:07 +0200

    feat: adds subtraction function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,6 @@ document.addEventListener("DOMContentLoaded",function() {
 
         })
     }
-    runGame("addition");
 })
 
 
@@ -34,6 +33,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === 'multiply') {
         displayMultiplyQuestion(num1, num2)
+    } else if (gameType === 'subtract') {
+        displaySubtractQuestion(num1, num2)
     } 
     
     
@@ -78,9 +79,12 @@ function calculateCorrectAnswer() {
 
     if (operator === '+'){
         return [operand1 + operand2, 'addition'];
-        
+
     }  else if (operator === 'x') {
-        return [operand1 + operand2, 'multiply'];
+        return [operand1 * operand2, 'multiply'];
+
+    } else if (operator === '-') {
+        return [operand1 - operand2, 'subtract'];
     }
     
     
@@ -115,8 +119,10 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
-    
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1,operand2) {
