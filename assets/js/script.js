680c89adf0a456039512e631e54a2bb84de8d69a
Steam Deck User <deck@steamdeck.station>
2023-08-11 22:51:34 +0200

    fix: faviconpage-title-enter_event-cursor_focus

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,15 @@ document.addEventListener("DOMContentLoaded",function() {
 
         })
     }
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter"){
+            checkAnswer();
+        }
+
+
+    })
+
+    runGame("addition");
 })
 
 
@@ -25,6 +34,10 @@ document.addEventListener("DOMContentLoaded",function() {
 
 
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     // Creates Two random number between 1 and 25
     let num1 = Math.floor(Math.random()*25)+1;
     let num2 = Math.floor(Math.random()*25)+1;
